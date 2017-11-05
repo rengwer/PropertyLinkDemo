b1b53706e9c22f1ae714df87e2d649581417ca1e
Robert Engwer <rengwer@gmail.com>
2017-11-05 15:36:05 -0500

    remove all

--- PropertyLinkDemo/Models/TextParserOptions.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace PropertyLinkDemo.Models
-{
-    public class TextParserOptions
-    {
-        public bool IgnoreHeaderLine { get; set; }
-        public bool IgnoreBlankLines { get; set; }
-    }
-}
